b227a610a285fdb37fcda1a218844166683781f1
Adam Paley <apaley95@gmail.com>
2024-10-15 11:38:43 -0400

    completed exercise 3

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/greetings/:name', (req, res) => {
 })
 
 // 2. Rolling the Dice
-// localhost:3000/roll/:isNumber
+// localhost:3000/roll/:number
 
 app.get('/roll/:number', (req, res) => {
     const number = Number(req.params.number)
@@ -34,7 +34,25 @@ app.get('/roll/:number', (req, res) => {
     } else {
         res.send(`You rolled a ${Math.floor(Math.random() * number)}.`)
     }
+})
 
+// 3. I Want THAT One!
+// localhost:3000/collectibiles/:index
+
+const collectibles = [
+    { name: 'shiny ball', price: 5.95 },
+    { name: 'autographed picture of a dog', price: 10 },
+    { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+];
+
+app.get('/collectibles/:index', (req, res) => {
+    const index = Number(req.params.index)
+
+    if (!collectibles[index]) {
+        return res.send('This item is not yet in stock. Check back soon!')
+    } else {
+        return res.send(`I see you eyeing that ${collectibles[index].name}. I'll give you a deal, just $${collectibles[index].price}!`)
+    }
 })
 
 // listen on port 3000 for request
