290207a6cb5d3aeda4f62f55aaec579ca7583fa0
Adam Paley <apaley95@gmail.com>
2024-10-15 11:25:28 -0400

    completed exercise 2

--- server.js
+++ server.js
@@ -20,10 +20,22 @@ app.get('/', (req, res) => {
 app.get('/greetings/:name', (req, res) => {
     const name = req.params.name
     formattedName = name.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
-    console.log(formattedName)
     res.send(`Hello, ${formattedName}. Would you like some fava beans and a nice chianti?`)
 })
 
+// 2. Rolling the Dice
+// localhost:3000/roll/:isNumber
+
+app.get('/roll/:number', (req, res) => {
+    const number = Number(req.params.number)
+
+    if (!number) {
+        return res.send("You must specify a number.")
+    } else {
+        res.send(`You rolled a ${Math.floor(Math.random() * number)}.`)
+    }
+
+})
 
 // listen on port 3000 for request
 app.listen(3000, () => {
